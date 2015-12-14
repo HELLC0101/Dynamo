bc53ef3a9e7f4e8d8afd20e56250a6e75aae3334
Ian Keough <ian.keough@autodesk.com>
2015-12-14 15:52:20 -0800

    Add data graph and tests.

---
+++ src/Libraries/DataGraph/Node.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace DataGraph
+{
+	/// <summary>
+	/// The Node class represents a nested array structure as
+	/// a tree where the nodes are arrays and the leaves are single objects.
+	/// </summary>
+	public class Node{
+
+		/// <summary>
+		/// Gets the address of the node in the tree as an array of 
+		/// ints representing the location of the node in each sub-array.
+		/// 
+		/// For example, 
+		/// </summary>
+		/// <value>The address.</value>
+		public List<uint> Address{ get;}
+
+		/// <summary>
+		/// Gets the level.
+		/// </summary>
+		/// <value>The level.</value>
+		public int Level{
+			get{return Address.Count;}
+		}
+
+		/// <summary>
+		/// Gets the Node which is the parent of this node.
+		/// </summary>
+		/// <value>A node, or null if the node is a root node.</value>
+		public Node Parent{ get; internal set;}
+
+		/// <summary>
+		/// Gets nodes which are children of this node.
+		/// </summary>
+		/// <value>The children.</value>
+		public List<Node> Children { get;}
+
+		/// <summary>
+		/// Gets a value indicating whether this instance is root.
+		/// </summary>
+		/// <value><c>true</c> if this instance is root; otherwise, <c>false</c>.</value>
+		public bool IsRoot{
+			get{return Parent == null;}
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether this instance has children.
+		/// </summary>
+		/// <value><c>true</c> if this instance has children; otherwise, <c>false</c>.</value>
+		public bool IsLeaf{
+			get{return Children.Count == 0;}
+		}
+
+		/// <summary>
+		/// The number of levels from this node to the leaves.
+		/// </summary>
+		/// <value>The depth.</value>
+		public int Depth{ get;}
+
+		/// <summary>
+		/// Gets the data associated with this node.
+		/// </summary>
+		/// <value>A reference to the original object or IEnumerable
+		/// stored at this position.</value>
+		public object Data{ get; private set;}
+
+		internal Node(object data, Node parent = null, uint index = 0){
+
+			Parent = parent;
+			Data = data;
+
+			Children = new List<Node> ();
+
+			Address = new List<uint> ();
+			if(parent != null){
+				Address.AddRange (parent.Address);
+			}
+			Address.Add (index);
+
+			if(data is IEnumerable && data.GetType() != typeof(string)){
+				uint count = 0;
+				foreach(var item in (IEnumerable)data){
+					Children.Add (new Node(item, this, count));
+					count++;
+				}
+			}
+
+			int depth = 0;
+			GetMaxDepth (ref depth,0);
+			Depth = depth;
+		}
+
+		public static Node FromData(object data)
+		{
+			if(data == null){
+				throw new Exception ("A node cannot be created with null data.");
+			}
+
+			return new Node (data);
+		}
+		
+		/// <summary>
+		/// Returns the depth of the graph from this node.
+		/// </summary>
+		/// <returns>The depth.</returns>
+		private void GetMaxDepth(ref int maxDepth, int currentDepth){
+			var newDepth = currentDepth+1;
+
+			if(Children.Count == 0){
+				maxDepth = Math.Max (newDepth, maxDepth);
+			}
+			foreach(var node in Children){
+				node.GetMaxDepth (ref maxDepth, newDepth);
+			}
+
+		}
+
+		/// <summary>
+		/// Traverse up the tree to find the root.
+		/// </summary>
+		/// <returns>The root.</returns>
+		public Node GetRoot(){
+			if(IsRoot){
+				return this;
+			}
+			else{
+				return Parent.GetRoot ();
+			}
+		}
+
+		/// <summary>
+		/// Gets the data at the specified level from node.
+		/// 
+		///        [] 	l0
+		///       / \
+		///      o  [] 	l1
+		///        /|\
+		///       o o o l2
+		/// 
+		/// If the level specified is greater than the available levels, a node is 
+		/// added to the root of the tree.
+		/// 
+		/// </summary>
+		/// <returns>The data at level from node.</returns>
+		/// <param name="root">The node from which the data gathering will begin.</param>
+		/// <param name="level">Level.</param>
+		public IEnumerable<object> GetDataAtLevel(int level)
+		{
+			if(level > Depth){
+				throw new Exception("The specified level is greater than the depth of the collection.");
+			}
+
+			var nodes = new List<Node> ();
+			GetNodesAtLevel (this, ref nodes, level);
+			return nodes.Select (n => n.Data);
+		}
+			
+		/// <summary>
+		/// Get all the nodes at the specified level, starting at the specified node.
+		/// 
+		/// If the level specified is a negative number then the root node will be replaced
+		/// with an additional root node.
+		/// </summary>
+		private static void GetNodesAtLevel(Node node, ref List<Node> gatheredNodes, int level){
+			if(level < 0){
+				var count = 0;
+				Node currentRoot = node;
+				while(count > level)
+				{
+					var newNode = new Node (new[]{currentRoot.Data});
+					currentRoot.Parent = newNode;
+					currentRoot = newNode;
+					count=count-1;
+				}
+				gatheredNodes.Add (currentRoot);
+				return;
+			}
+
+			if(node.Level == level){
+				gatheredNodes.Add (node);
+				return;
+			}else{
+				foreach (var n in node.Children) {
+					GetNodesAtLevel (n, ref gatheredNodes, level);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Get all leaf nodes from the specified node.
+		/// </summary>
+		/// <param name="node">Node.</param>
+		/// <param name="gatheredLeaves">Gathered leaves.</param>
+		public List<Node> GetAllLeafNodes(){
+			var gatheredLeaves = new List<Node>();
+			GetAllLeafNodesImpl (this, ref gatheredLeaves);
+			return gatheredLeaves;
+		}
+
+		private static void GetAllLeafNodesImpl(Node node, ref List<Node> gatheredLeaves){
+			if(node.IsLeaf){
+				gatheredLeaves.Add (node);
+			}
+
+			foreach(var c in node.Children){
+				GetAllLeafNodesImpl (c, ref gatheredLeaves);
+			}
+		}
+
+		/// <summary>
+		/// Get all data associated with leaf nodes from the specified node.
+		/// </summary>
+		/// <returns>The all leaf data.</returns>
+		/// <param name="node">Node.</param>
+		public static IEnumerable<object> GetAllLeafData(Node node){
+			var leaves = node.GetAllLeafNodes ();
+			return leaves.Select (n => n.Data);
+		}
+
+		/// <summary>
+		/// Clones the node and returns a node with the same topology, 
+		/// but full of nulls.
+		/// </summary>
+		public Node CloneWithNulls()
+        {
+			var newNode = (Node)this.MemberwiseClone ();
+			newNode.NullData();
+			return newNode;
+		}
+
+        /// <summary>
+        /// Clones the node and returns a node with the same topology,
+        /// with nulls at all positions on a given level.
+        /// </summary>
+        /// <param name="level"></param>
+        /// <returns></returns>
+	    public Node CloneWithNullsAtLevel(int level)
+	    {
+	        var newNode = (Node) this.MemberwiseClone();
+            newNode.NullDataAtLevel(level);
+	        return newNode;
+	    }
+
+		private void NullData()
+		{
+		    Data = Children.Count == 0 ? null : 
+                Enumerable.Repeat<object> (null, Children.Count);
+
+		    foreach(var c in Children){
+				c.NullData ();
+			}
+		}
+
+	    private void NullDataAtLevel(int level)
+	    {
+	        if (Level == level)
+	        {
+	            Data = Children.Count == 0 ? null : 
+                    Enumerable.Repeat<object>(null, Children.Count);
+            }
+
+	        foreach (var c in Children)
+	        {
+	            c.NullDataAtLevel(level);
+	        }
+	    }
+
+		/// <summary>
+		/// Give a node with a different structure, update the structure of 
+		/// this node to match, filling in previously non-existing branches with nulls.
+		/// 
+		/// If the structure to superimpose is a subset of this node's graph,
+		/// then no changes will be made.
+		/// </summary>
+		public void Superimpose(Node structure){
+
+
+		}
+			
+		public override string ToString ()
+		{
+			return string.Join (".",Address.Select (x => x.ToString ()));
+		}
+
+
+	}
+}
+
---
+++ test/Libraries/DataGraphTests/NodeTests.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using DataGraph;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace DataGraphTests
+{
+	public class DataGraphTests
+	{
+		private string[] arr1 = new[]{"A","B","C"}; 
+		private int[] arr2 = new[]{0,1,2}; 
+		private string[] arr3 = new[]{"a","b","c"};
+
+		[Test]
+		public void Level_SingleDimension(){
+			var node = new Node (SingleDimensionalTestArray ());
+			Assert.AreEqual (node.Level, 1);
+			Assert.AreEqual (node.Children.First ().Level, 2);
+		}
+
+		[Test]
+		public void Level_Jagged(){
+			var node = new Node (JaggedTestArray ());
+			Assert.AreEqual (node.Children.First ().Level, 2);
+			Assert.AreEqual (node.Children.Last ().Level, 2);
+		}
+
+		[Test]
+		public void ToString_SingleDimension(){
+
+			var node = new Node (SingleDimensionalTestArray ());
+			Assert.AreEqual (node.ToString (), "0");
+		}
+
+		[Test]
+		public void ToString_TwoDimension(){
+			var node = new Node (TwoDimensionalTestArray ());
+			var data = node.GetAllLeafNodes ();
+			Assert.AreEqual (data.First ().ToString (), "0.0.0");
+			Assert.AreEqual (data.Last ().ToString (), "0.1.2");
+		}
+
+		[Test]
+		public void ToString_Jagged(){
+			var node = new Node (JaggedTestArray ());
+			var data = node.GetAllLeafNodes ();
+			Assert.AreEqual (data.First ().ToString (), "0.0");
+			Assert.AreEqual (data.Last ().ToString (), "0.2.2");
+		}
+
+		[Test]
+		public void Depth_SingleDimension(){
+			var node1 = new Node (SingleDimensionalTestArray ());
+			Assert.AreEqual (2, node1.Depth);
+		}
+
+		[Test]
+		public void Depth_TwoDimension(){
+			var node2 = new Node (TwoDimensionalTestArray ());
+			Assert.AreEqual (3, node2.Depth);
+		}
+
+		[Test]
+		public void Depth_Jagged(){
+			var node2 = new Node (JaggedTestArray ());
+			Assert.AreEqual (3, node2.Depth);
+		}
+
+		[Test]
+		public void GetAllLeafData_SingleDimension()
+		{
+			var node = new Node (SingleDimensionalTestArray ());
+			var data = Node.GetAllLeafData (node);
+			Assert.AreEqual(data.Count (), 3);
+		}
+
+		[Test]
+		public void GetAllLeafData_TwoDimension(){
+			var node = new Node (TwoDimensionalTestArray ());
+			var data = Node.GetAllLeafData (node);
+			Assert.AreEqual(data.Count (), 6);
+		}
+
+		[Test]
+		public void GetAllLeafData_Jagged(){
+			var node = new Node (JaggedTestArray ());
+			var data = Node.GetAllLeafData (node);
+			Assert.AreEqual(data.Count (), 7);
+		}
+
+		[Test]
+		public void GetDataAtLevel_Jagged(){
+			var node = new Node (JaggedTestArray ());
+			Console.WriteLine (PrintData (node.Data));
+			var data = node.GetDataAtLevel (node.Depth-2);
+			Console.WriteLine ("@-2");
+			Console.WriteLine (PrintData (data));
+		    var denum = (IEnumerable<object>)data.First();
+            Assert.AreEqual(denum.First(), "foo");
+		}
+
+		[Test]
+		public void GetDataAtLevel_ThreeDimension(){
+			var node = new Node (ThreeDimensionalTestArray ());
+			Console.WriteLine (PrintData (node.Data));
+			var data = node.GetDataAtLevel (node.Depth - 1);
+			Console.WriteLine ("@-1");
+			Assert.AreEqual (data.First (), "A");
+			Console.WriteLine (PrintData (data));
+			Assert.AreEqual (data.Last (), arr3);
+		}
+
+		[Test]
+		public void GetDataAtLevel_Jagged_AtLevel(){
+			var node = new Node (JaggedTestArray ());
+			Console.WriteLine (PrintData (node.Data));
+			var data = node.GetDataAtLevel (node.Depth);
+			Console.WriteLine ("@depth");
+			Assert.AreEqual (data.First (), arr1.First ());
+			Console.WriteLine (PrintData (data));
+			Assert.AreEqual (data.Last (), arr2.Last ());
+		}
+
+		[Test]
+		public void GetDataAtLevel_Jagged_Leaves(){
+			var node = new Node (JaggedTestArray ());
+			Console.WriteLine (PrintData (node.Data));
+			var data = Node.GetAllLeafData (node);
+			Console.WriteLine ("@leaves");
+			Assert.AreEqual (data.First (), "foo");
+			Console.WriteLine (PrintData (data));
+			Assert.AreEqual (data.Last (), arr2.Last ());
+		}
+
+		[Test]
+		public void GetDataAtLevel_NegativeIndex(){
+			var node = new Node (JaggedTestArray ());
+			Console.WriteLine (PrintData (node.Data));
+			var initalDepth = node.Depth;
+			var data = node.GetDataAtLevel (node.Depth-5);
+			Console.WriteLine ("@-5");
+			Console.WriteLine (PrintData (data));
+			Assert.AreEqual (5, node.GetRoot ().Depth);
+		}
+
+		[Test]
+		public void GetDataAtLevel_DepthGreaterThanSize_ThrowsException(){
+			var node = new Node (JaggedTestArray ());
+			Assert.Throws<Exception> (()=>node.GetDataAtLevel (10));
+		}
+
+		[Test]
+		public void GetDataAtLevel_Case1(){
+			var node = new Node (Phase2 ());
+
+			var data = node.GetDataAtLevel (node.Depth-2);
+			Assert.AreEqual (data.Count (), 2);
+
+			data = node.GetDataAtLevel (node.Depth-1);
+			Assert.AreEqual (data.Count (), 4);
+
+			data = node.GetDataAtLevel (node.Depth);
+			Assert.AreEqual (data.Count (), 12);
+		}
+
+		[Test]
+		public void GetAllLeafData_IsLeaves_CorrectCount(){
+			var node = new Node (JaggedTestArray ());
+			var data = Node.GetAllLeafData (node);
+			Assert.AreEqual(7, data.Count ());
+		}
+
+		[Test]
+		public void CloneWithNulls_Jagged(){
+			var node = new Node (JaggedTestArray ());
+		    var nodeWithNulls = node.CloneWithNulls();
+			var leaves = nodeWithNulls.GetAllLeafNodes ();
+			foreach(var l in leaves){
+				Assert.AreEqual(null, l.Data);
+			}
+		}
+
+        [Test]
+        public void CloneWithNullsAtLevel_Jagged()
+        {
+            var node = new Node(JaggedTestArray());
+            var nodeWithNulls = node.CloneWithNullsAtLevel(node.Depth - 2);
+            var leafData = nodeWithNulls.GetDataAtLevel(node.Depth-2);
+            foreach (var l in leafData)
+            {
+                if (l is IEnumerable)
+                {
+                    Assert.AreEqual(l, Enumerable.Repeat<object>(null, 3));
+                }
+                else
+                {
+                    Assert.AreEqual(null, l);
+                }
+            }
+        }
+
+        private IEnumerable SingleDimensionalTestArray(){
+			return arr1;
+		}
+
+		private IEnumerable TwoDimensionalTestArray(){
+			return new List<object>(){arr1,arr2};
+		}
+
+		private IEnumerable ThreeDimensionalTestArray(){
+			return new List<object>{arr1,arr2,new[]{arr3, arr3}};
+		}
+			
+		private IEnumerable JaggedTestArray(){
+			return new List<object>{ "foo", arr1, arr2 };
+		}
+
+		private IEnumerable Phase2(){
+			return new [] {
+				new[] {
+					new[]{ "A", "B", "C" },
+					new[]{ "D", "E", "F" },
+				},
+				new[] {
+					new[]{ "a", "b", "c" },
+					new[]{ "d", "e", "f" },
+				},
+			};
+		}
+
+		private string PrintData(object data){
+			var jsonString = JsonConvert.SerializeObject (
+				data, Formatting.Indented);
+			return jsonString;
+		}
+	}
+}
+
