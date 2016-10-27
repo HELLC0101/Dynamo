a9b609d8234ffd083434f89f7ab3808b745211a9
Ian Keough <ian.keough@autodesk.com>
2016-10-27 11:25:00 -0700

    Move NodeToCode methods to NodeToCode class.

--- src/DynamoCore/Engine/NodeToCode/NodeToCode.cs
+++ src/DynamoCore/Engine/NodeToCode/NodeToCode.cs
@@ -1,20 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Dynamo.Models;
 using System;
-using Autodesk.DesignScript.Geometry;
 using ProtoCore.AST.AssociativeAST;
 using ProtoCore.DSASM;
 using Dynamo.Utilities;
 using ProtoCore.SyntaxAnalysis;
-using ProtoCore.Mirror;
 using ProtoCore.Utils;
 using Dynamo.Core;
 using ProtoCore.Namespace;
 using Dynamo.Engine.CodeGeneration;
 using Dynamo.Graph;
 using Dynamo.Graph.Nodes;
-using Dynamo.Interfaces;
+using Dynamo.Graph.Connectors;
+using Dynamo.Graph.Workspaces;
 
 namespace Dynamo.Engine.NodeToCode
 {
@@ -1339,6 +1337,88 @@ namespace Dynamo.Engine.NodeToCode
 
             var result = new NodeToCodeResult(allAstNodes.SelectMany(x => x.Item2), inputMap, outputMap);
             return result;
+        }
+    }
+
+    internal class NodeToCode
+    {
+        /// <summary>
+        /// Checks whether the given connection is inside the node to code set or outside it.
+        /// This determines if it should be redrawn(if it is external) or if it should be
+        /// deleted (if it is internal)
+        /// </summary>
+        internal static bool IsInternalNodeToCodeConnection(IEnumerable<NodeModel> nodes, ConnectorModel connector)
+        {
+            return nodes.Contains(connector.Start.Owner) && nodes.Contains(connector.End.Owner);
+        }
+
+        /// <summary>
+        /// Forms new connections from the external nodes to the Node To Code Node,
+        /// based on the connectors passed as inputs.
+        /// </summary>
+        /// <param name="externalOutputConnections">List of connectors to remake, along with the port names of the new port</param>
+        /// <param name="cbn">The new Node To Code created Code Block Node</param>
+        internal static List<ConnectorModel> ReConnectOutputConnections(Dictionary<ConnectorModel, string> externalOutputConnections, 
+            CodeBlockNodeModel cbn)
+        {
+            List<ConnectorModel> newConnectors = new List<ConnectorModel>();
+            foreach (var kvp in externalOutputConnections)
+            {
+                var connector = kvp.Key;
+                var variableName = kvp.Value;
+
+                //Get the start and end idex for the ports for the connection
+                var portModel = cbn.OutPorts.FirstOrDefault(
+                    port => cbn.GetRawAstIdentifierForOutputIndex(port.Index).Value.Equals(variableName));
+
+                if (portModel == null)
+                    continue;
+
+                //Make the new connection and then record and add it
+                var newConnector = ConnectorModel.Make(
+                    cbn,
+                    connector.End.Owner,
+                    portModel.Index,
+                    connector.End.Index);
+
+                newConnectors.Add(newConnector);
+            }
+            return newConnectors;
+        }
+
+        /// <summary>
+        /// Forms new connections from the external nodes to the Node To Code Node,
+        /// based on the connectors passed as inputs.
+        /// </summary>
+        /// <param name="externalInputConnections">List of connectors to remake, along with the port names of the new port</param>
+        /// <param name="cbn">The new Node To Code created Code Block Node</param>
+        internal static List<ConnectorModel> ReConnectInputConnections(
+            Dictionary<ConnectorModel, string> externalInputConnections, CodeBlockNodeModel cbn, WorkspaceModel workspace)
+        {
+            List<ConnectorModel> newConnectors = new List<ConnectorModel>();
+
+            foreach (var kvp in externalInputConnections)
+            {
+                var connector = kvp.Key;
+                var variableName = kvp.Value;
+
+                var endPortIndex = CodeBlockNodeModel.GetInportIndex(cbn, variableName);
+                if (endPortIndex < 0)
+                    continue;
+
+                if (workspace.Connectors.Any(c => c.End == cbn.InPorts[endPortIndex]))
+                    continue;
+
+                var newConnector = ConnectorModel.Make(
+                    connector.Start.Owner,
+                    cbn,
+                    connector.Start.Index,
+                    endPortIndex);
+
+                newConnectors.Add(newConnector);
+            }
+
+            return newConnectors;
         }
     }
 }
--- src/DynamoCore/Graph/Workspaces/WorkspaceModel.cs
+++ src/DynamoCore/Graph/Workspaces/WorkspaceModel.cs
@@ -1518,7 +1518,7 @@ namespace Dynamo.Graph.Workspaces
 
                         foreach (var connector in node.AllConnectors.ToList())
                         {
-                            if (!IsInternalNodeToCodeConnection(nodeList, connector))
+                            if (!NodeToCode.IsInternalNodeToCodeConnection(nodeList, connector))
                             {
                                 //If the connector is an external connector, the save its details
                                 //for recreation later
@@ -1580,13 +1580,13 @@ namespace Dynamo.Graph.Workspaces
                     #endregion
 
                     #region Step III. Recreate the necessary connections
-                    var newInputConnectors = ReConnectInputConnections(externalInputConnections, codeBlockNode);
+                    var newInputConnectors = NodeToCode.ReConnectInputConnections(externalInputConnections, codeBlockNode, this);
                     foreach (var connector in newInputConnectors)
                     {
                         undoHelper.RecordCreation(connector);
                     }
 
-                    var newOutputConnectors = ReConnectOutputConnections(externalOutputConnections, codeBlockNode);
+                    var newOutputConnectors = NodeToCode.ReConnectOutputConnections(externalOutputConnections, codeBlockNode);
                     foreach (var connector in newOutputConnectors)
                     {
                         undoHelper.RecordCreation(connector);
@@ -2066,88 +2066,6 @@ namespace Dynamo.Graph.Workspaces
             }
 
             return foundModels;
-        }
-
-        #endregion
-
-        #region Node To Code Reconnection
-
-        /// <summary>
-        /// Checks whether the given connection is inside the node to code set or outside it.
-        /// This determines if it should be redrawn(if it is external) or if it should be
-        /// deleted (if it is internal)
-        /// </summary>
-        private static bool IsInternalNodeToCodeConnection(IEnumerable<NodeModel> nodes, ConnectorModel connector)
-        {
-            return nodes.Contains(connector.Start.Owner) && nodes.Contains(connector.End.Owner);
-        }
-
-        /// <summary>
-        /// Forms new connections from the external nodes to the Node To Code Node,
-        /// based on the connectors passed as inputs.
-        /// </summary>
-        /// <param name="externalOutputConnections">List of connectors to remake, along with the port names of the new port</param>
-        /// <param name="cbn">The new Node To Code created Code Block Node</param>
-        private List<ConnectorModel> ReConnectOutputConnections(Dictionary<ConnectorModel, string> externalOutputConnections, CodeBlockNodeModel cbn)
-        {
-            List<ConnectorModel> newConnectors = new List<ConnectorModel>();
-            foreach (var kvp in externalOutputConnections)
-            {
-                var connector = kvp.Key;
-                var variableName = kvp.Value;
-
-                //Get the start and end idex for the ports for the connection
-                var portModel = cbn.OutPorts.FirstOrDefault(
-                    port => cbn.GetRawAstIdentifierForOutputIndex(port.Index).Value.Equals(variableName));
-
-                if (portModel == null)
-                    continue;
-
-                //Make the new connection and then record and add it
-                var newConnector = ConnectorModel.Make(
-                    cbn,
-                    connector.End.Owner,
-                    portModel.Index,
-                    connector.End.Index);
-
-                newConnectors.Add(newConnector);
-            }
-            return newConnectors;
-        }
-
-        /// <summary>
-        /// Forms new connections from the external nodes to the Node To Code Node,
-        /// based on the connectors passed as inputs.
-        /// </summary>
-        /// <param name="externalInputConnections">List of connectors to remake, along with the port names of the new port</param>
-        /// <param name="cbn">The new Node To Code created Code Block Node</param>
-        private List<ConnectorModel> ReConnectInputConnections(
-            Dictionary<ConnectorModel, string> externalInputConnections, CodeBlockNodeModel cbn)
-        {
-            List<ConnectorModel> newConnectors = new List<ConnectorModel>();
-
-            foreach (var kvp in externalInputConnections)
-            {
-                var connector = kvp.Key;
-                var variableName = kvp.Value;
-
-                var endPortIndex = CodeBlockNodeModel.GetInportIndex(cbn, variableName);
-                if (endPortIndex < 0)
-                    continue;
-
-                if (Connectors.Any(c => c.End == cbn.InPorts[endPortIndex]))
-                    continue;
-
-                var newConnector = ConnectorModel.Make(
-                    connector.Start.Owner,
-                    cbn,
-                    connector.Start.Index,
-                    endPortIndex);
-
-                newConnectors.Add(newConnector);
-            }
-
-            return newConnectors;
         }
 
         #endregion
